3b718926c769b62ba9cdaaf5469e03de1be61194
firekeeper <philip.maasch@gmail.com>
2025-04-10 19:30:28 +0200

    feat: Add authentication tests and create post test; improve test coverage for user roles

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -11,6 +11,9 @@ class AuthenticationTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Test if the login screen can be rendered.
+     */
     public function test_login_screen_can_be_rendered(): void
     {
         $response = $this->get('/login');
@@ -20,8 +23,12 @@ class AuthenticationTest extends TestCase
             ->assertSeeVolt('pages.auth.login');
     }
 
+    /**
+     * Test if users can authenticate using the login screen.
+     */
     public function test_users_can_authenticate_using_the_login_screen(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $component = Volt::test('pages.auth.login')
@@ -37,8 +44,12 @@ class AuthenticationTest extends TestCase
         $this->assertAuthenticated();
     }
 
+    /**
+     * Test if users can not authenticate with an invalid email.
+     */
     public function test_users_can_not_authenticate_with_invalid_password(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $component = Volt::test('pages.auth.login')
@@ -54,8 +65,12 @@ class AuthenticationTest extends TestCase
         $this->assertGuest();
     }
 
+    /**
+     * Test if users can not authenticate with an invalid email.
+     */
     public function test_navigation_menu_can_be_rendered(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $this->actingAs($user);
@@ -67,8 +82,12 @@ class AuthenticationTest extends TestCase
             ->assertSeeVolt('layout.navigation');
     }
 
+    /**
+     * Test if users can logout.
+     */
     public function test_users_can_logout(): void
     {
+        /** @var \App\Models\User $user */
         $user = User::factory()->create();
 
         $this->actingAs($user);
